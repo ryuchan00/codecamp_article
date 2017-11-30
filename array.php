44c8214677c364009a0470ba3f5bd0a2546dbf7b
ryuchun00 <leokun0210@gmail.com>
2017-12-01 00:29:00 +0900

    配列と連想配列の比較用のコードが完成

---
+++ array.php
@@ -0,0 +1,19 @@
+<?php
+
+// 配列
+$vegetable = [
+    "carrot",
+    "tomato",
+    "lettuce"
+];
+
+echo $vegetable[0], PHP_EOL;
+
+// 連想配列
+$vegetable_color = [
+    "carrot" => "orange",
+    "tomato" => "red",
+    "lettuce" => "green"
+];
+
+echo $vegetable_color["carrot"], PHP_EOL;
