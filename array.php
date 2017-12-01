ab3fbcf7283d8a0dfb2e159a43c2ad1f08505be5
ryuchun00 <leokun0210@gmail.com>
2017-12-01 09:02:23 +0900

    配列、連想配列の検証コードをコーディング

--- array.php
+++ array.php
@@ -7,7 +7,14 @@ $vegetable = [
     "lettuce"
 ];
 
+// 添え字に整数を用いる
 echo $vegetable[0], PHP_EOL;
+
+// 新しく要素を追加する時
+$vegetable[] = "potato";
+
+var_dump($vegetable);
+
 
 // 連想配列
 $vegetable_color = [
@@ -16,4 +23,10 @@ $vegetable_color = [
     "lettuce" => "green"
 ];
 
+// 添え字に文字列を用いる
 echo $vegetable_color["carrot"], PHP_EOL;
+
+// 新しく要素を追加する時
+$vegetable_color["potato"] = "brown";
+
+var_dump($vegetable_color);
