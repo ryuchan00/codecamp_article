b9717c6dc95485cc784fec225407ce65e16989eb
ryuchun00 <leokun0210@gmail.com>
2017-12-05 09:03:14 +0900

    foreachのサンプルコード記述

---
+++ array_foreach.php
@@ -0,0 +1,12 @@
+<?php
+$array = [
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "seventeen" => 17
+];
+
+foreach ($array as $key => $value) {
+    echo "\$array[$key] => $value.", PHP_EOL;
+}
+?>
---
+++ array_foreach2.php
@@ -0,0 +1,14 @@
+<?php
+$array = [
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "seventeen" => 17
+];
+
+// $valueに各要素を代入
+foreach ($array as $value) {
+    echo "$value.", PHP_EOL;
+}
+?>
+a
