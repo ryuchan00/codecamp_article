2b61b7ab6cd2d56032cf2e3c6c49b9af499e1ad1
ryuchun00 <leokun0210@gmail.com>
2018-03-13 01:22:45 +0900

    preg_matchをより実務的に

--- preg_match.php
+++ preg_match.php
@@ -1,11 +1,11 @@
 <?php
-preg_match('/(foo)(baz)(\w+)/', 'foobarbazaa', $matches, PREG_OFFSET_CAPTURE);
-echo "<pre>";
-print_r($matches);
-echo "</pre>";
+$pattern = '/(php)[\s\w]*(language)/i';
+$mystring = "PHP is the web scripting language of choice.";
 // パターンのデリミタの後の "i" は、大小文字を区別しない検索を示す
-if (preg_match("/php/i", "PHP is the web scripting language of choice.")) {
-    echo "A match was found.";
+if (preg_match($pattern, $mystring, $matches)) {
+    echo "<pre>";
+    print_r($matches);
+    echo "</pre>";
 } else {
-    echo "A match was not found.";
+    echo "'$mystring'の中には見つかりませんでした";
 }
