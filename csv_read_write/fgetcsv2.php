7ce20c9a1f11d807dd959f66b44b131e591a1896
ryuchun00 <leokun0210@gmail.com>
2018-04-04 01:24:19 +0900

    サンプルコードを進化

--- csv_read_write/fgetcsv2.php
+++ csv_read_write/fgetcsv2.php
@@ -1,9 +1,18 @@
-<table>
 <?php
-// 1行ずつ読み込み、表組みに生成します。
-while ($arr = fgetcsv($fp)) {
+// ロケールを設定します。CSVファイルをUTF-8で読み込みます。
+setlocale(LC_ALL, 'ja_JP.UTF-8');
 
+$row = 1;
+if (($handle = fopen("test.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        echo "${row}行目のフィールドは${num}個あります。\n";
+        $row++;
+        for ($c=0; $c < $num; $c++) {
+            echo "「${data[$c]}」です。\n";
+        }
+    }
+    fclose($handle);
 }
 ?>
-</table>
 
