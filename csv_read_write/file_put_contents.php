f75a2733c29cfd6b752b3456e30611313cc116b7
ryuchun00 <leokun0210@gmail.com>
2018-04-06 00:01:09 +0900

    SplFileObjectについての知見が深まった

--- csv_read_write/file_put_contents.php
+++ csv_read_write/file_put_contents.php
@@ -1,9 +1,6 @@
 <?php
-//$line = "${_POST['column1']}, ${_POST['column2']}";
-$line = <<<CSV
-aaa
-bbb,ccc
-CSV;
+// colomun1の中で改行している。
+$line = "{$_POST['column1']}, {$_POST['column2']}";
 
 // 結果をファイルに書き出します
 file_put_contents('file3.csv', $line);
---
+++ csv_read_write/splFileObject.php
@@ -0,0 +1,17 @@
+<?php
+// インプットのCSVのオブジェクトを生成する
+$inputFile = new SplFileObject("animals.csv");
+// 同じくーアウトプットのCSVのオブジェクトを生成する
+$outputFile = new SplFileObject('output_animals.csv', 'w');
+
+// setFlags()関数はSplFileObjectによって使われるフラグをセットする
+// READ_CSCはCSV 列として行を読み込む
+$inputFile->setFlags(SplFileObject::READ_CSV);
+// オブジェクトをforeach構文で回すだけで、fopne()関数を使わなくても1行ずつ読み込むことができる
+foreach ($inputFile as $row) {
+    list($animal, $legs) = $row;
+    // 読み込んだCSVを元に文章を作成する
+    $sentence = sprintf("%sの足は%d本ある", $animal, $legs);
+    // SplFileObject::fputcsv()関数を使って、optput_animals.csvに書き出す。
+    $outputFile->fputcsv(array($animal, $legs, $sentence));
+}
