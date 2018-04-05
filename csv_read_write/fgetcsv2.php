25884002524549686d75f08941c5071b11a474ed
ryuchun00 <leokun0210@gmail.com>
2018-04-05 09:44:36 +0900

    Add fputcsv

--- csv_read_write/fgetcsv2.php
+++ csv_read_write/fgetcsv2.php
@@ -1,15 +1,19 @@
 <?php
+// 文字化け対策
+//$tempCSV = file_get_contents($csvFile);
+// 文字エンコードをUTF-8に変換します
+//$tempCSV = mb_convert_encoding($tempCSV, 'UTF¥8','CP932')
+
 // ロケールを設定します。CSVファイルをUTF-8で読み込みます。
-setlocale(LC_ALL, 'ja_JP.UTF-8');
+//setlocale(LC_ALL, 'ja_JP.UTF-8');
 
 $row = 1;
 if (($handle = fopen("test.csv", "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $num = count($data);
-        echo "${row}行目のフィールドは${num}個あります。\n";
+    while (($data = fgetcsv($handle))) {
+        echo "${row}行目\n";
         $row++;
-        for ($c=0; $c < $num; $c++) {
-            echo "「${data[$c]}」です。\n";
+        foreach ($data as $value) {
+            echo "「${value}」\n";
         }
     }
     fclose($handle);
