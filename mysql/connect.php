12a4ee4eb12560fda29cb0304043bbf288751c34
ryuchun00 <leokun0210@gmail.com>
2018-04-14 08:48:47 +0900

    記事用のコードに編集

--- mysql/connect.php
+++ mysql/connect.php
@@ -1,10 +1,13 @@
 <?php
 
 $link = mysqli_connect('127.0.0.1', 'root', 'root', 'codecamp');
+//$link = mysqli_connect('localhost', 'user', 'pass', 'shop');
 
 // 接続状況をチェックします
 if (mysqli_connect_errno()) {
     die("データベースに接続できません:" . mysqli_connect_error() . "\n");
+} else {
+    echo "データベースの接続に成功しました。\n";
 }
 
 $query = 'SELECT * FROM user';
