431e9dd44ae5e351c4cf30ebb233524d80eba763
ryuchun00 <leokun0210@gmail.com>
2018-04-21 11:37:13 +0900

    接続成功の文字を入れた

--- pdo/pdo_connect.php
+++ pdo/pdo_connect.php
@@ -7,7 +7,8 @@ $password = 'root';
 
 try {
     $dbh = new PDO($dsn, $user, $password);
+    echo "接続成功\n";
 } catch (PDOException $e) {
-    echo '接続失敗: ' . $e->getMessage() . "\n";
+    echo "接続失敗: " . $e->getMessage() . "\n";
 }
 
