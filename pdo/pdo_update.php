3523925254b682a504da71d6c5d68a0ef75f4b85
ryuchun00 <leokun0210@gmail.com>
2018-04-22 12:48:16 +0900

    updateの処理を追加ˆ

---
+++ pdo/pdo_update.php
@@ -0,0 +1,31 @@
+<?php
+
+// ドライバ呼び出しを使用して MySQL データベースに接続する
+$dsn = 'mysql:dbname=shop;host=127.0.0.1';
+$user = 'root';
+$password = 'root';
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    echo "接続成功\n";
+} catch (PDOException $e) {
+    echo "接続失敗: " . $e->getMessage() . "\n";
+    exit();
+}
+
+// SQL文を準備します。「:id」「:name」がプレースホルダーです。
+$sql = 'UPDATE user SET name=:name WHERE id=:id';
+$prepare = $dbh->prepare($sql);
+
+$prepare->bindValue(':id', 4, PDO::PARAM_INT);
+$prepare->bindValue(':name', 'kikuchi', PDO::PARAM_STR);
+$prepare->execute();
+
+// INSERTされたデータを確認
+$sql = 'SELECT * FROM user';
+$prepare = $dbh->prepare($sql);
+
+$prepare->execute();
+
+$result = $prepare->fetchAll(PDO::FETCH_NUM);
+var_dump($result);
