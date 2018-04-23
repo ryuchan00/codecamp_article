667e19411f82e0707db5062df024d202fdd048a8
ryuchun00 <leokun0210@gmail.com>
2018-04-23 23:01:36 +0900

    コメントを追加

--- pdo/pdo_select.php
+++ pdo/pdo_select.php
@@ -1,9 +1,11 @@
 <?php
 
+// 先ほど作成したPDOインスタンス作成をそのまま使用する
 require 'pdo_connect.php';
 
 // SQL文を準備します。「:id」がプレースホルダーです。
 $sql = 'SELECT * FROM user WHERE id < :id';
+// PDOStatementクラスのインスタンスを生成
 $prepare = $dbh->prepare($sql);
 
 $prepare->bindValue(':id', 3, PDO::PARAM_INT);
