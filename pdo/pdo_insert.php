dbf290e764a72d4be87555986eab13b9a91bb83a
ryuchun00 <leokun0210@gmail.com>
2018-04-23 22:57:48 +0900

    connect部分を共通化

--- pdo/pdo_insert.php
+++ pdo/pdo_insert.php
@@ -1,17 +1,6 @@
 <?php
 
-// ドライバ呼び出しを使用して MySQL データベースに接続する
-$dsn = 'mysql:dbname=shop;host=127.0.0.1';
-$user = 'root';
-$password = 'root';
-
-try {
-    $dbh = new PDO($dsn, $user, $password);
-    echo "接続成功\n";
-} catch (PDOException $e) {
-    echo "接続失敗: " . $e->getMessage() . "\n";
-    exit();
-}
+require 'pdo_connect.php';
 
 // SQL文を準備します。「:id」「:name」がプレースホルダーです。
 $sql = 'INSERT INTO user (id, name) VALUE (:id, :name)';
